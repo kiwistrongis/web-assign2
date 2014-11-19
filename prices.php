834ae56745cab17d7f4d95cf062d7f6c0ee26513
kurotetsuka <kurotetsuka@gmail.com>
2014-11-18 19:55:26 -0500

    started working on web dev assignment 2

---
+++ prices.php
@@ -0,0 +1,9 @@
+<?xml version="1.0">
+<prices>
+	<laptop name='Acer Aspire V7' price='$700' />
+	<laptop name='Dell Inspiron' price='$600' />
+	<laptop name='Lenovo Yoga 2' price='$1100' />
+	<laptop name='MacBook Pro Retina' price='$1400' />
+	<laptop name='MacBook Air' price='$900' />
+	<laptop name='Sony VAIO Pro 13' price='$1400' />
+</prices>
---
+++ tool/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo rm -rf /srv/http/*
+sudo cp prices.php /srv/http/
+#sudo chmod og+r -R /srv/http
